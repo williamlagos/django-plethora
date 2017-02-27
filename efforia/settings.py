84ee91c1ae30b7b74b748cf64a9621587267dab7
William Oliveira de Lagos <william.lagos@live.com>
2017-02-27 22:52:43 +0200

    Cleaned old structure to prepare Python3/Django1.10 compliance

--- efforia/settings.py
+++ efforia/settings.py
@@ -1,8 +1,6 @@
 import os
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 TIME_ZONE = 'America/Sao_Paulo'
 LANGUAGE_CODE = 'pt-br'
 SITE_ID = 1
@@ -22,12 +20,32 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-TEMPLATE_LOADERS = (
-    ('efforia.jade.ext.django.Loader',(
-    	'django.template.loaders.filesystem.Loader',
-    	'django.template.loaders.app_directories.Loader',
-    )),
-)
+TEMPLATES = [
+    {
+        'DEBUG': True,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.abspath('static'),
+        ],
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request'
+            ],
+            'loaders': [
+                ('pypugjs.ext.django.Loader', (
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ))
+            ],
+            'builtins': ['pypugjs.ext.django.templatetags'],
+        },
+    },
+]
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -37,25 +55,18 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'urls'
 WSGI_APPLICATION = 'efforia.wsgi.application'
-TEMPLATE_DIRS = (
-    os.path.abspath('static'),
-)
 
 INSTALLED_APPS = [
+    'flat_responsive',
     'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'efforia',
-    'efforia.bootstrap',
-    'django.contrib.admin',
-    'efforia.paypal',
-    'efforia.pagseguro',
     'efforia.infinite',
-    'gunicorn',
-    'south'
 ]
 
 LOGGING = {
@@ -91,3 +102,71 @@ EFFORIA_APPS = ()
 EFFORIA_OBJS = {}
 EFFORIA_NAMES = {}
 EFFORIA_TOKENS = {}
+
+ADMINS = (('William Oliveira de Lagos', 'william@efforia.com.br'),)
+MANAGERS = ADMINS
+SECRET_KEY = 'x5dvfbk$u-(07^f1229p*_%rcuc+nka45j6awo==*jkyjiucql'
+
+#DATABASES = {
+#  'default': {
+#    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#    'NAME': 'd6i6a0klirtqjd',
+#    'HOST': 'ec2-54-243-243-176.compute-1.amazonaws.com',
+#    'PORT': 5432,
+#    'USER': 'kkoaphemdgvutt',
+#    'PASSWORD': 'ztTIw8EcIYX2UlNolrVmTb8yZQ'
+#  }
+#}
+
+DATABASES = {
+  'default': {
+     'ENGINE':'django.db.backends.sqlite3',
+     'NAME':'dev.db'
+   }
+}
+
+#EFFORIA_APPS = ('spread','promote')
+EFFORIA_APPS = ('plethora',)
+EFFORIA_OBJS.update({
+    'spread':  ['Playable','Spreadable','Image','Product'],
+#    'promote': ['Project','Event']
+})
+EFFORIA_NAMES.update({
+    'spread':  ('Espalhe','spread'),
+#    'promote': ('Promova','promote')
+})
+EFFORIA_TOKENS.update({
+    "@": "Profile",
+#    "#": "Project",
+#    "@#":"Project",
+#    "##":"Movement",
+    ">": "Playable",
+    ">!":"Playable",
+    "!": "Spreadable",
+    "!!":"Spreadable",
+#    "@!":"Event",
+#    "@": "Event",
+#    "@@":"Event",
+    "%": "Image",
+    "%!":"Image",
+    "!#":"Page",
+    "!%":"Image",
+    "$$":"Product"
+})
+INSTALLED_APPS.extend(EFFORIA_APPS)
+STATICFILES_DIRS.extend((
+    os.path.abspath('spread/public'),
+#    os.path.abspath('promote/public'),
+))
+
+
+PAYPAL_RECEIVER_EMAIL = 'caokzu@gmail.com'
+PAYPAL_NOTIFY_URL = '/paypal'
+PAYPAL_RETURN_URL = '/'
+PAYPAL_CANCEL_RETURN = '/cancel'
+
+PAGSEGURO_EMAIL_COBRANCA = 'contato@efforia.com.br'
+PAGSEGURO_TOKEN = '1a3ea7wq2e7eq8e1e223add23ad23'
+PAGSEGURO_URL_RETORNO = '/pagseguro/retorno/'
+PAGSEGURO_URL_FINAL = '/obrigado/'
+PAGSEGURO_ERRO_LOG  = '/tmp/pagseguro_erro.log'
