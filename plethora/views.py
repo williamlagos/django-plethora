8654e601fa89516845010858308083f9e7bfaa1a
William Oliveira de Lagos <william.lagos@icloud.com>
2020-05-15 23:21:50 -0300

    Phased out old urls and relinked view methods to new service class

--- plethora/views.py
+++ plethora/views.py
@@ -18,81 +18,80 @@
 # along with Plethora. If not, see <http://www.gnu.org/licenses/>.
 #
 
-from .app import Images,Spreads,Uploads
-from .content import Spreadables
-# from .models import Product
-
 from django.views import View
 from django.http import JsonResponse
 
+from .services import *
+
 class ContentsView(View):
+    
     def get(self, request):
         return JsonResponse({'contents': 'success'})
 
-def start(request):
-    e = Feedly()
-    if request.method == 'GET':
-        return e.start(request)
+    def start(self, request):
+        e = ContentService()
+        if request.method == 'GET':
+            return e.start(request)
 
-def media(request):
-    u = Uploads()
-    if request.method == 'GET':
-        return u.media_chooser(request)
+    def media(self, request):
+        u = ContentService()
+        if request.method == 'GET':
+            return u.media_chooser(request)
 
-def spreaded(request):
-    s = Spreadables()
-    if request.method == 'GET':
-        return s.view_spreaded(request)
+    def spreaded(self, request):
+        s = ContentService()
+        if request.method == 'GET':
+            return s.view_spreaded(request)
 
-def spreadspread(request):
-    s = Spreadables()
-    if request.method == 'GET':
-        return s.spreadspread(request)
-    elif request.method == 'POST':
-        return s.spreadobject(request)
+    def spreadspread(self, request):
+        s = ContentService()
+        if request.method == 'GET':
+            return s.spreadspread(request)
+        elif request.method == 'POST':
+            return s.spreadobject(request)
 
-def spreadable(request):
-    s = Spreadables()
-    if request.method == 'GET':
-        return s.view_spreadable(request)
+    def spreadable(self, request):
+        s = ContentService()
+        if request.method == 'GET':
+            return s.view_spreadable(request)
 
-def playable(request):
-    s = Spreadables()
-    if request.method == 'GET':
-        return s.view_playable(request)
+    def playable(self, request):
+        s = ContentService()
+        if request.method == 'GET':
+            return s.view_playable(request)
 
-def imageview(request):
-    s = Spreadables()
-    if request.method == 'GET':
-        return s.view_images(request)
+    def imageview(self, request):
+        s = ContentService()
+        if request.method == 'GET':
+            return s.view_images(request)
 
-def image(request):
-    i = Images()
-    if request.method == 'GET':
-        return i.view_image(request)
-    elif request.method == 'POST':
-        return i.create_image(request)
+    def image(self, request):
+        i = ContentService()
+        if request.method == 'GET':
+            return i.view_image(request)
+        elif request.method == 'POST':
+            return i.create_image(request)
 
-def upload(request):
-    u = Uploads()
-    if request.method == 'GET':
-        return u.view_content(request)
-    elif request.method == 'POST':
-        return u.upload_content(request)
+    def upload(self, request):
+        u = ContentService()
+        if request.method == 'GET':
+            return u.view_content(request)
+        elif request.method == 'POST':
+            return u.upload_content(request)
 
-def init_spread(request):
-    spread = Spreads()
-    if request.method == 'GET':
-        return spread.start_spreadapp(request)
+    def init_spread(self, request):
+        spread = ContentService()
+        if request.method == 'GET':
+            return spread.start_spreadapp(request)
 
-def main(request):
-    graph = Spreads()
-    if request.method == 'GET':
-        return graph.view_spread(request)
-    elif request.method == 'POST':
-        return graph.create_spread(request)
+    def main(self, request):
+        graph = ContentService()
+        if request.method == 'GET':
+            return graph.view_spread(request)
+        elif request.method == 'POST':
+            return graph.create_spread(request)
 
-def content(request):
-    upload = Uploads()
-    if request.method == 'GET':
-        return upload.view_upload(request)
+    def content(self, request):
+        upload = ContentService()
+        if request.method == 'GET':
+            return upload.view_upload(request)
